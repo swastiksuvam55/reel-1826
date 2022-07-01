5dfa2eea1388d4041db201111871c17dfd92c81a
sekharjavvadi <sekharatece@gmail.com>
2022-07-01 12:59:06 +0530

    scroll effects implemented

--- pages/homepage.jsx
+++ pages/homepage.jsx
@@ -1,12 +1,44 @@
 import Image from "next/image";
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 export default function HomePage() {
+  const imageRef = useRef(null);
+  const [changeCol, setchangeCol] = useState(false);
+
+  const onScrollMethod = (e) => {
+    if (e.target.scrollTop >= imageRef.current.clientHeight * 0.23) {
+      if (!changeCol) setchangeCol(true);
+    } else {
+      if (changeCol) setchangeCol(false);
+    }
+  };
+
+  const roadMap = () => {
+    console.log(imageRef);
+    document.getElementById("divElem").scrollTop =
+      imageRef.current.clientHeight * 0.25;
+    // setchangeCol(true);
+  };
+  const onClickStudio = () => {
+    document.getElementById("divElem").scrollTop = 0;
+    // setchangeCol(false);
+  };
+
   return (
     <div className="home-page">
       <div className="nav-bar row">
-        <p>Studio</p>
-        <p>Roadmap</p>
+        <p
+          onClick={onClickStudio}
+          className={changeCol ? "pointer" : "pointer white-bg"}
+        >
+          Studio
+        </p>
+        <p
+          className={changeCol ? "pointer white-bg" : "pointer"}
+          onClick={roadMap}
+        >
+          Roadmap
+        </p>
       </div>
       <div className="body">
         <div className="title-parent">
@@ -17,8 +49,8 @@ export default function HomePage() {
           </p>
         </div>
 
-        <div className="image-parent">
-          <img src="/reel.png" className="image" />
+        <div className="image-parent" id="divElem" onScroll={onScrollMethod}>
+          <img src="/reel.png" className="image" ref={imageRef} />
         </div>
       </div>
     </div>
